596c7d416dc468288f335197e8db93a60124cb27
Om salokhe <omsalokhe2020@gmail.com>
2025-12-29 09:56:14 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -9,74 +9,82 @@ Script Purpose:
 ===============================================================================
 */
 
-CREATE OR ALTER PROCEDURE bronze.load_bronze AS
-	BEGIN TRY
-		TRUNCATE TABLE bronze.crm_cust_info;
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
+GO
 
-		BULK INSERT bronze.crm_cust_info
-		FROM 'F:\Backend\database\sql\microsoftsqlserver\DWH\dataset\source_crm\cust_info.csv'
-		WITH(
-			FIRSTROW=2,
-			FIELDTERMINATOR=',',
-			TABLOCK
-		);
+CREATE TABLE bronze.crm_cust_info (
+    cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE
+);
+GO
 
-		TRUNCATE TABLE bronze.crm_prd_info;
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
+GO
 
-		BULK INSERT bronze.crm_prd_info
-		FROM 'F:\Backend\database\sql\microsoftsqlserver\DWH\dataset\source_crm\prd_info.csv'
-		WITH(
-			FIRSTROW=2,
-			FIELDTERMINATOR=',',
-			TABLOCK
-		);
+CREATE TABLE bronze.crm_prd_info (
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
+);
+GO
 
-		TRUNCATE TABLE bronze.crm_sales_details;
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details;
+GO
 
-		BULK INSERT bronze.crm_sales_details
-		FROM 'F:\Backend\database\sql\microsoftsqlserver\DWH\dataset\source_crm\sales_details.csv'
-		WITH(
-			FIRSTROW=2,
-			FIELDTERMINATOR=',',
-			TABLOCK
-		);
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
+);
+GO
 
-		TRUNCATE TABLE bronze.erp_CUST_AZ12;
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101;
+GO
 
-		BULK INSERT bronze.erp_CUST_AZ12
-		FROM 'F:\Backend\database\sql\microsoftsqlserver\DWH\dataset\source_erp\CUST_AZ12.csv'
-		WITH(
-			FIRSTROW=2,
-			FIELDTERMINATOR=',',
-			TABLOCK
-		);
+CREATE TABLE bronze.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50)
+);
+GO
 
-		TRUNCATE TABLE bronze.erp_LOC_A101;
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12;
+GO
 
-		BULK INSERT bronze.erp_LOC_A101
-		FROM 'F:\Backend\database\sql\microsoftsqlserver\DWH\dataset\source_erp\LOC_A101.csv'
-		WITH(
-			FIRSTROW=2,
-			FIELDTERMINATOR=',',
-			TABLOCK
-		);
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
+);
+GO
 
-		TRUNCATE TABLE bronze.erp_PX_CAT_G1V2;
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2;
+GO
 
-		BULK INSERT bronze.erp_PX_CAT_G1V2
-		FROM 'F:\Backend\database\sql\microsoftsqlserver\DWH\dataset\source_erp\PX_CAT_G1V2.csv'
-		WITH(
-			FIRSTROW=2,
-			FIELDTERMINATOR=',',
-			TABLOCK
-		);
-	END TRY
-
-	BEGIN CATCH
-	PRINT '=========================='
-	PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
-	PRINT 'ERROR MESSAGE '+ERROR_MESSAGE(); 
-	PRINT 'ERROR MESSAGE '+CAST(ERROR_NUMBER() AS NVARCHAR);
-	PRINT '=========================='
-	END CATCH
-END
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
+);
+GO
